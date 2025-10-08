588e88cd126b5543f716ddfad280e1c55285acc1
Viktor Johansson <vj222hx@student.lnu.se>
2025-10-08 12:57:55 +0200

    dataset structure

---
+++ dataset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Dataset:
+    data = None
+    features = None
+    
+    def __init__(self, path):
+        self.data = pd.read_csv(path)
+        
+    def __len__(self):
+        return len(self.data)
+
+    def getFeatures(self):
+        return self.data.columns.tolist()
+    
+    def getData(self):
+        return self.data.values.tolist()
