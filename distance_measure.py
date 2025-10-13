f061e23919cfc189bafaaf38bfe0b08174063750
Viktor Johansson <vj222hx@student.lnu.se>
2025-10-13 11:09:33 +0200

    name changes

--- distance_measure.py
+++ distance_measure.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class distance_measure_clustering:
+class distance_measure:
     
     def euclidean_distance(point1, point2):
         point1, point2 = np.array(point1), np.array(point2)
